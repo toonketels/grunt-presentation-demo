cd84e1d0a64ff7632a559471a0e88011455f385a
Toon Ketels <toon.ketels@wunderkraut.com>
2013-05-14 21:29:10 +0200

    Added get task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,16 @@ module.exports = function(grunt) {
     introspect: {
       configOne: 'config one',
       configTwo: [1, 2]
+    },
+    get: {
+      'node-json': {
+        'url': "http://nodejs.org/api/all.json",
+        'name': 'node-docs.json'
+      },
+      'node-html': {
+        'url': "http://nodejs.org/api/all.html",
+        'name': 'node-docs.html'
+      }
     }
   });
 
@@ -82,4 +92,43 @@ module.exports = function(grunt) {
     
     console.log(grunt.option.flags());
   });
+
+
+
+  grunt.registerMultiTask('get', 'Perform get request', function() {
+
+    var http = require('http'),
+        fs = require('fs'),
+        config = grunt.config.get(this.name)[this.target],
+        url =  config.url,
+        name = config.name,
+        prefix =  grunt.template.today('yyyy-mm-dd-hh-MM-ss'),
+        dir =  'downloads',
+        file = grunt.template.process("<%= dir %>/<%= prefix %>-<%= name %>",{ data: {dir: dir, prefix: prefix, name: name }}),
+        done = this.async(),
+        fd;
+    
+    http.get(url, function(res) {
+
+      grunt.log.writeln("Got response: " + res.statusCode);
+      grunt.log.writeln("Downloading...");
+
+      res.setEncoding('utf8');
+      fd = fs.openSync(file, 'w');
+      
+      res.on('data', function (chunk) {
+        fs.writeSync(fd, chunk);
+        grunt.verbose.writeln("Wrote chunk...");
+      });
+      
+      res.on('end', function() {
+          grunt.log.ok("Loaded");
+          fs.closeSync(fd);
+          done();
+      });
+    }).on('error', function(e) {
+      grunt.fail.fatal("Got error: " + e.message);
+    });
+  });
+
 };
